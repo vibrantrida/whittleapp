09aa5814c147790c0bd53caa4e80400dd5c4aadf
vibrantrida <vibrantsmug@gmail.com>
2019-11-26 16:46:14 +0800

    add electron-updater

--- src/background.js
+++ src/background.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, protocol, BrowserWindow } from 'electron'
+import { autoUpdater } from 'electron-updater'
 import {
   createProtocol,
   installVueDevtools
@@ -38,6 +39,7 @@ function createWindow () {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
+    autoUpdater.checkForUpdatesAndNotify()
   }
 
   win.on('closed', () => {
