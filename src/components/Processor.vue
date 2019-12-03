313316f1ce62e4aabc9499a13e3afe7231d7b229
vibrantrida <vibrantsmug@gmail.com>
2019-12-04 03:39:02 +0800

    support smackjeeves

--- src/components/Processor.vue
+++ src/components/Processor.vue
@@ -70,8 +70,9 @@ export default {
         if (isPNG || isJPEG) {
           let tapas = await this.generateStrip(940, 4000, '.png', 'tapas')
           let webtoons = await this.generateStrip(800, 1280, '.jpg', 'webtoons')
+          let smackjeeves = await this.generateStrip(690, 1280, '.png', 'smackjeeves')
 
-          if (tapas && webtoons) {
+          if (tapas && webtoons && smackjeeves) {
             this.state = 2
           }
         } else {
